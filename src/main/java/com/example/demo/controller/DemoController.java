4caff1e8afd794d75df72247c356a3fc68c4dd8f
dev-nikhil-kumar <nikhilbhati1223@gmail.com>
2026-08-03 18:06:28 +0530

    intial commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+public class DemoController {
+
+    @PostMapping("/demo")
+    public List<String> toList(@RequestBody String str){
+        if (str == null || str.isBlank()) {
+            return Collections.emptyList();
+        }
+
+        return Arrays.stream(str.split(","))
+                .map(String::trim)
+                .toList(); // Use .collect(Collectors.toList()) if on Java 16 or lower
+    }
+}
